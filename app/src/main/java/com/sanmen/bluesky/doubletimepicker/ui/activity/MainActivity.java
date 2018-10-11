98367a3d9adf6c657df1bbea6a273286c49f6bd0
lxt_bluesky <864464536@qq.com>
2018-10-11 16:21:52 +0800

    Initial commit

---
+++ app/src/main/java/com/sanmen/bluesky/doubletimepicker/ui/activity/MainActivity.java
@@ -0,0 +1,67 @@
+package com.sanmen.bluesky.doubletimepicker.ui.activity;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.sanmen.bluesky.doubletimepicker.R;
+import com.sanmen.bluesky.doubletimepicker.widget.DoubleTimeSelectDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDialog;
+
+    DoubleTimeSelectDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initLayout();
+    }
+
+    private void initLayout() {
+
+        btnDialog = findViewById(R.id.button);
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDoubleTimePicker();
+            }
+        });
+        //设置最大进度
+        //        roundProgressView.setMaxProgress(80);
+        //        roundProgressView.setBaseUnit("fen");
+        //        roundProgressView.setBorderWidth(16);
+        //        roundProgressView.setTextSize(16);
+        //        roundProgressView.setBorderColor(Color.BLUE);
+        //        roundProgressView.setStartPosition(RoundProgressView.LEFT);
+
+    }
+
+    private void showDoubleTimePicker() {
+        if (dialog==null){
+            dialog=new DoubleTimeSelectDialog(this,"2017-07-07","2030-09-31","2010-09-31");
+            dialog.setType(DoubleTimeSelectDialog.TYPE.YEAR_MONTH);
+            dialog.setOnDateSelectFinished(new DoubleTimeSelectDialog.OnDateSelectFinished() {
+                @Override
+                public void onSelectFinished(String startTime, String endTime) {
+
+                }
+            });
+
+            dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface d) {
+                    dialog=null;
+                }
+            });
+
+            if (!dialog.isShowing()) {
+                dialog.show();
+            }
+        }
+    }
+}
